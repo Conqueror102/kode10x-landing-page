99ecd5db82aac9d8251daffdbb44ca0ee9a1813b
Davidokih <davidyole023@gmail.com>
2024-12-18 11:27:14 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        pc: { max: "906px" },
+        tablet: { max: "768px" },
+        mobileTab: { max: "620px" },
+        mobile: { max: "425px" },
+        phone: { max: "375px" },
+        sphone: { max: "350px" },
+        radio: { max: "320px" },
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
   },
   plugins: [],
-}
+};
